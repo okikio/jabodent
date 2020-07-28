545d5adf0377bbbdfe2f2536dff7b7f4d030a52e
Okiki <femikiki@gmail.com>
2020-07-28 04:14:50 +0000

    add polyfills and overlay to splashscreen,
    now work on samsung internet

--- build/ts/services/Splashscreen.ts
+++ build/ts/services/Splashscreen.ts
@@ -6,6 +6,7 @@ export class Splashscreen extends Service {
   protected innerEl: HTMLElement;
   protected bgEl: HTMLElement;
   protected minimalDuration: number = 1000; // ms
+  protected coverEl: HTMLElement;
 
   public init() {
     super.init();
@@ -16,36 +17,40 @@ export class Splashscreen extends Service {
     )[0] as HTMLElement;
 
     if (this.rootElement) {
+      this.coverEl = this.rootElement.querySelector(".splashscreen-cover");
       this.innerEl = this.rootElement.querySelector(".splashscreen-inner");
     }
   }
 
-  public async boot() {
+  public boot() {
     if (this.rootElement) {
-      let anim = this.rootElement.getAnimations()[0];
-      let innerElAnim = this.innerEl.getAnimations()[0];
+      if (typeof this.coverEl.getAnimations === "function") {
+        let coverElAnim = this.coverEl.getAnimations()[0];
+        let innerElAnim = this.innerEl.getAnimations()[0];
+        console.log(coverElAnim.pause);
+        // coverElAnim.pause();
+        innerElAnim.pause();
 
-      anim.pause();
-      innerElAnim.pause();
+        // coverElAnim.currentTime = this.minimalDuration;
+        innerElAnim.currentTime = this.minimalDuration;
+        this.EventEmitter.emit("BEFORE_SPLASHSCREEN_HIDE");
 
-      anim.currentTime = this.minimalDuration;
-      this.EventEmitter.emit("BEFORE_SPLASHSCREEN_HIDE");
+        coverElAnim.onfinish = () => {
+          this.coverEl.style.opacity = `0`;
+        };
 
-      anim.onfinish = () => {
-        this.rootElement.style.transform = "translateY(100%)";
-        this.rootElement.style.visibility = "hidden";
-        this.rootElement.style.pointerEvents = "none";
-      };
+        innerElAnim.onfinish = () => {
+          this.innerEl.style.transform = "translateY(100%)";
+          this.rootElement.style.visibility = "hidden";
+          this.rootElement.style.pointerEvents = "none";
+        };
 
-      innerElAnim.onfinish = () => {
-        this.innerEl.style.opacity = `0`;
-      };
-
-      window.setTimeout(() => {
-        anim.play();
-        innerElAnim.play();
-        this.EventEmitter.emit("START_SPLASHSCREEN_HIDE");
-      }, this.minimalDuration);
+        window.setTimeout(() => {
+          coverElAnim.play();
+          innerElAnim.play();
+          this.EventEmitter.emit("START_SPLASHSCREEN_HIDE");
+        }, this.minimalDuration);
+      }
     }
   }
 }
