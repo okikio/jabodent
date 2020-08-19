9d9b49784b9627e8ff16559dc69ab2c685d52570
Okiki <femikiki@gmail.com>
2020-08-19 17:23:07 +0000

    es6

--- gulpfile.js
+++ gulpfile.js
@@ -266,7 +266,7 @@ tasks({
                             esbuild({
                                 // watch: watching,
                                 minify: true,
-                                target: "es2016", // default, or 'es20XX', 'esnext'
+                                target: "es2015", // default, or 'es20XX', 'esnext'
                             }),
                         ],
                         onwarn,
